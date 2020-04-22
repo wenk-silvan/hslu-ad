ed26f6f57da6d145101abb0413484f5d0b2d12c4
wenksi <silvan.wenk@gmail.com>
2020-04-22 17:44:03 +0200

    SW10: Aufgaben 6 - Bubble Sort

--- src/main/java/ch/hslu/ad/sw10/Sort.java
+++ src/main/java/ch/hslu/ad/sw10/Sort.java
@@ -1,6 +1,6 @@
 package ch.hslu.ad.sw10;
 
-public class Sort {
+class Sort {
     static int[] insertionSort(final int[] array) {
         int element;
         int j;
@@ -12,6 +12,38 @@ public class Sort {
              j--;
          }
          array[j] = element;
+        }
+        return array;
+    }
+
+    static int[] selectionSort(final int[] array) {
+        int indexOfSmallest;
+        for (int i = 0; i < array.length; i++) {
+            indexOfSmallest = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[indexOfSmallest]) {
+                    indexOfSmallest = j;
+                }
+            }
+            var cache = array[i];
+            array[i] = array[indexOfSmallest];
+            array[indexOfSmallest] = cache;
+        }
+        return array;
+    }
+
+    static int[] bubbleSort(final int[] array) {
+        int current;
+        for (int i = array.length; i >= 0; i--) {
+            current = 0;
+            for (int next = 1; next < i; next++) {
+                if (array[current] > array[next]) {
+                    var cache = array[current];
+                    array[current] = array[next];
+                    array[next] = cache;
+                }
+                current++;
+            }
         }
         return array;
     }
--- src/test/java/ch/hslu/ad/sw10/SortTest.java
+++ src/test/java/ch/hslu/ad/sw10/SortTest.java
@@ -9,14 +9,16 @@ import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class SortTest {
+class SortTest {
     private static final Logger LOG = LogManager.getLogger(SortTest.class);
+    private final int[] shortArray = {3, 1, 5, 0, 19, -1, 4, 3};
+    private final int[] mediumArray = generateRandomArray(10000);
+    private final int[] longArray = generateRandomArray(100000);
 
     @Test
     void testInsertionSortShort() {
-        int[] array = {3, 1, 5, 0, 19, -1, 4, 3};
         int[] expected = {-1, 0, 1, 3, 3, 4, 5, 19};
-        var actual = (new Timer<int[]>()).stopWatch(LOG, () -> Sort.insertionSort(array));
+        var actual = (new Timer<int[]>()).stopWatch(LOG, () -> Sort.insertionSort(this.shortArray));
         for (int i = 0; i < expected.length; i++) {
             assertEquals(expected[i], actual[i]);
         }
@@ -24,14 +26,50 @@ public class SortTest {
 
     @Test
     void testInsertionSortMedium() {
-        var array = generateRandomArray(10000);
-        var actual = (new Timer<int[]>()).stopWatch(LOG, () -> Sort.insertionSort(array));
+        var actual = (new Timer<int[]>()).stopWatch(LOG, () -> Sort.insertionSort(this.mediumArray));
     }
 
     @Test
     void testInsertionSortLong() {
-        var array = generateRandomArray(100000);
-        var actual = (new Timer<int[]>()).stopWatch(LOG, () -> Sort.insertionSort(array));
+        var actual = (new Timer<int[]>()).stopWatch(LOG, () -> Sort.insertionSort(this.longArray));
+    }
+
+    @Test
+    void testSelectionSortShort() {
+        int[] expected = {-1, 0, 1, 3, 3, 4, 5, 19};
+        var actual = (new Timer<int[]>()).stopWatch(LOG, () -> Sort.selectionSort(this.shortArray));
+        for (int i = 0; i < expected.length; i++) {
+            assertEquals(expected[i], actual[i]);
+        }
+    }
+
+    @Test
+    void testSelectionSortMedium() {
+        var actual = (new Timer<int[]>()).stopWatch(LOG, () -> Sort.selectionSort(this.mediumArray));
+    }
+
+    @Test
+    void testSelectionSortLong() {
+        var actual = (new Timer<int[]>()).stopWatch(LOG, () -> Sort.selectionSort(this.longArray));
+    }
+
+    @Test
+    void testBubbleSortShort() {
+        int[] expected = {-1, 0, 1, 3, 3, 4, 5, 19};
+        var actual = (new Timer<int[]>()).stopWatch(LOG, () -> Sort.bubbleSort(this.shortArray));
+        for (int i = 0; i < expected.length; i++) {
+            assertEquals(expected[i], actual[i]);
+        }
+    }
+
+    @Test
+    void testBubbleSortMedium() {
+        var actual = (new Timer<int[]>()).stopWatch(LOG, () -> Sort.bubbleSort(this.mediumArray));
+    }
+
+    @Test
+    void testBubbleSortLong() {
+        var actual = (new Timer<int[]>()).stopWatch(LOG, () -> Sort.bubbleSort(this.longArray));
     }
 
     private int[] generateRandomArray(int n){
